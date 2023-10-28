cf5dcd2cb266c77bc8410423fe3e1ff460495658
hirimi <azizhilmyhaidar@gmail.com>
2023-10-28 23:42:49 +0700

    update

---
+++ core/src/main/java/com/example/core/utils/SharedObject.kt
@@ -0,0 +1,9 @@
+package com.example.core.utils
+
+object SharedObject {
+    const val hostname = "themoviedb.org"
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val certificatePinner1 = "sha256/5VLcahb6x4EvvFrCF2TePZulWqrLHS2jCg9Ywv6JHog="
+    const val certificatePinner2 = "sha256/vxRon/El5KuI4vx5ey1DgmsYmRY0nDd5Cg4GfJ8S+bg="
+    const val certificatePinner3 = "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI="
+}
